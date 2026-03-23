722cb1ff3266033c6415ad639764bc50dc1e6340
mr.star <93962086+xccvme@users.noreply.github.com>
2026-03-23 11:45:48 +0700

    Add files via upload

---
+++ sshws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+systemctl stop ws
+curl -sS ipv4.icanhazip.com > /usr/bin/.ipvps
+REPO="https://raw.githubusercontent.com/xccvme/ssh-ws/main/"
+wget -O /usr/bin/ws "${REPO}ws_mod2"
+wget -O /usr/bin/config.conf "${REPO}config.conf"
+wget -O /etc/systemd/system/ws.service "${REPO}ws.service"
+chmod +x /usr/bin/ws
+systemctl daemon-reload
+systemctl enable ws.service
+systemctl start ws.service
+systemctl restart ws.service
